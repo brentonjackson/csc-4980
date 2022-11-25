e14f3c6fb89d57cf12d681886689d63fb9832339
Brenton Jackson <bjavonjackson@gmail.com>
2022-11-25 05:37:55 -0500

    move image capture to cature_img.py and accept image argument in find_obj_dims.py

--- Assignment1/find_obj_dims.py
+++ Assignment1/find_obj_dims.py
@@ -20,50 +20,34 @@
 # viewport position Vy
 # viewport position Vz = d
 
-import numpy as np
 import cv2 as cv
 import depthai as dai
+import argparse
+import subprocess
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=False, help="Path to the image")
+args = vars(ap.parse_args())
+
+# 1. Grab image with object in it
+print(args)
+# if no image argument specified, capture new image
+if (not args["image"]) :
+    subprocess.run(['python3', 'capture_img.py'])
+
+imgName = args["image"] or "opencv_frame1.png"
 
 
-# 1. capture image of object with camera
-# Create pipeline
-pipeline = dai.Pipeline()
+# 2. Find height of object (in image) by allowing user to select two points on image
 
-# Define source and output
-camRgb = pipeline.create(dai.node.ColorCamera)
-xoutRgb = pipeline.create(dai.node.XLinkOut)
+# load the image, clone it, and setup the mouse callback function
+image = cv.imread(imgName)
+clone = image.copy()
+cv.namedWindow("image")
+# cv.setMouseCallback("image", capture_height)
 
-xoutRgb.setStreamName("rgb")
+cv.imshow("object img", image)
 
-# Properties
-camRgb.setPreviewSize(400, 400)
-camRgb.setInterleaved(False)
-camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-
-# Linking
-camRgb.preview.link(xoutRgb.input)
-
-# Connect to device and start pipeline
-with dai.Device(pipeline) as device:
-    # Output queue will be used to get the rgb frames from the output defined above
-    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-
-    while True:
-        inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
-
-        # Retrieve 'bgr' (opencv format) frame
-        frame = inRgb.getCvFrame()
-        cv.imshow("rgb", frame)
-        
-        k = cv.waitKey(1)
-        if k%256 == 32:
-            # SPACE pressed, save image
-            img_name = "opencv_frame1.png"
-            cv.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-
-        if k == ord('q'):
-            break
-
-
-cv.destroyAllWindows()
+# process two click events by clicking and using spacebar to accept click
+k = cv.waitKey(0)
