157a9c6879e7751eeb3a3323bb9e75abffc3a961
Brenton Jackson <bjavonjackson@gmail.com>
2022-12-01 15:40:22 -0500

    correct comment

--- Assignment4/get_obj_dims.py
+++ Assignment4/get_obj_dims.py
@@ -66,7 +66,7 @@ spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
 spatialDetectionNetwork.setDepthLowerThreshold(100)
 spatialDetectionNetwork.setDepthUpperThreshold(5000)
 
-objectTracker.setDetectionLabelsToTrack([5])  # track only person
+objectTracker.setDetectionLabelsToTrack([5])  # track only bottle
 # possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
 objectTracker.setTrackerType(dai.TrackerType.ZERO_TERM_COLOR_HISTOGRAM)
 # take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
